6ca1c152cd8ab49f6ccfb8bf69498e4fd5578e88
Kirill <kirilllola@gmail.com>
2018-11-22 00:05:53 +0200

    first commit dev branch

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+	<div id="app">
+		<MainNav></MainNav>
+		<transition name="fade-up" mode="out-in">
+			<router-view/>
+		</transition>
+	</div>
+</template>
+
+<script>
+  import MainNav from './components/MainNav'
+
+  export default {
+    name: 'App',
+    components: {
+      MainNav,
+    },
+  }
+</script>
+
+<style lang="scss">
+	@import "https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700";
+	@import '~bootstrap/scss/bootstrap.scss';
+
+	#app {
+		font-family: 'Roboto', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		color: #2c3e50;
+	}
+
+	h1 {
+		text-align: center;
+	}
+
+	.fade-up-enter-active, .fade-up-leave-active {
+		transition: .5s;
+		transform: translate(0,0);
+	}
+
+	.fade-up-enter, .fade-up-leave-to {
+		transform: translate(0,-20px);
+		opacity: 0;
+	}
+
+	.items-up-enter-active, .items-up-leave-active {
+		transition: .5s;
+		transform: translate(0,0);
+	}
+
+	.items-up-enter, .items-up-leave-to {
+		transform: translate(20px,0);
+		height: 0;
+		opacity: 0;
+	}
+
+	.items-up-list-move {
+		transition: transform 1s;
+	}
+</style>
---
+++ src/components/UserList.vue
@@ -0,0 +1,74 @@
+<template>
+	<div>
+		<table class="table table-hover">
+			<thead>
+			<tr>
+				<th>
+					User id
+				</th>
+				<th>
+					First name
+				</th>
+				<th>
+					Last name
+				</th>
+				<th>
+					Balance
+				</th>
+				<th>
+					Delete
+				</th>
+			</tr>
+			</thead>
+			<tbody>
+			<tr v-for="item in list" :key="item.id">
+				<td>
+					<router-link :to="'/edit-user/' + item.id">{{ item.id }}</router-link>
+				</td>
+				<td>
+					{{ item.firstName }}
+				</td>
+				<td>
+					{{ item.lastName }}
+				</td>
+				<td>
+					{{ item.balance }}
+				</td>
+				<td>
+					<button type="button" @click="removeUser(item.id)">remove</button>
+				</td>
+			</tr>
+			</tbody>
+		</table>
+	</div>
+</template>
+
+<script>
+  export default {
+    name: 'UserList',
+    props: {
+      list: {
+        type: Array,
+        required: true
+      },
+      title: {
+        type: String,
+        default: 'test message (not require)'
+      }
+    },
+    data () {
+      return {
+        userListtest: '123'
+      }
+    },
+    methods: {
+      removeUser: function (id) {
+        this.$emit('remove', id)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
